5ad36c8f112bc125beab536126c32ae8017337af
Brandon Lyons <brandonl.4231@gmail.com>
2021-11-26 19:34:08 -0500

    Add attack logic, update deploy

--- scripts/run.js
+++ scripts/run.js
@@ -12,27 +12,24 @@ const main = async () => {
     // HP Values
     [150, 150, 200, 150],
     // Attack Values
-    [45, 50, 40, 55]
+    [45, 50, 40, 55],
+    "Team Rocket", // Boss Name
+    "https://cdn.vox-cdn.com/thumbor/Ffrvj4ltYfBKJni5dvdYaaGrGP4=/0x0:1432x1080/920x613/filters:focal(616x165:844x393):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/64687982/chrome_2019_07_08_14_17_05.0.jpg", // Boss image
+    1000, // Boss hp
+    30 // Boss attack
   );
   await gameContract.deployed();
   console.log(`Contract deployed to: ${gameContract.address}`);
 
   let txn;
-  txn = await gameContract.mintCharacterNFT(0);
-  await txn.wait();
-  console.log("Minted NFT #1");
-
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log("Minted NFT #2");
-
   txn = await gameContract.mintCharacterNFT(2);
   await txn.wait();
-  console.log("Minted NFT #3");
 
-  txn = await gameContract.mintCharacterNFT(3);
+  txn = await gameContract.attackBoss();
   await txn.wait();
-  console.log("Minted NFT #4");
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
 
   console.log("Done deploying and minting!");
 };
